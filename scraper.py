30536351f4ded2ee34915caad9860ac08f0c5ef2
iPrathamKumkar <prathameshkumkar@gmail.com>
2020-03-08 03:18:46 -0700

    Crawled 20 pages

--- scraper.py
+++ scraper.py
@@ -1,20 +1,27 @@
 import scrapy
-import collections
+import time
 
 class Scraper(scrapy.Spider):
     name  = "conference_spider"
-    start_urls = ["http://www.wikicfp.com/cfp/call?conference=big%20data%20&page=1"]
+    page_number = 15
+    start_urls = ["http://www.wikicfp.com/cfp/call?conference=big%20data%20&page=15"]
 
     def parse(self, response):
+        Scraper.page_number += 1
         rows = response.xpath("//html//body//div[4]//center//form//table//tr[3]//td//table//tr")
 
         i = 1
-        j = 2
-        while j < len(rows):
+        while i + 1 < len(rows):
             conference = dict()
-            conference['acronym'] = rows[i].css("a::text")[0].extract()
-            conference['title'] = rows[i].css("td::text")[0].extract()
-            conference['location']= rows[j].css("td::text")[1].extract()
-            i += 2
-            j += 2
-            yield conference
+            if len(rows[i].css("a::text")) > 0:
+                conference['acronym'] = rows[i].css("a::text")[0].extract()
+                conference['title'] = rows[i].css("td::text")[0].extract()
+                conference['location']= rows[i + 1].css("td::text")[1].extract()
+                i += 2
+                yield conference
+            else:
+                i += 1
+        time.sleep(7)
+        next_page = "http://www.wikicfp.com/cfp/call?conference=big%20data%20&page=" + str(Scraper.page_number)
+        if Scraper.page_number <= 20:
+            yield response.follow(next_page, callback = self.parse)

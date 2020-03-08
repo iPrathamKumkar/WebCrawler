d9ac7e0f0136058f4bacd7cf375d44dc8558cc03
iPrathamKumkar <prathameshkumkar@gmail.com>
2020-03-07 21:15:46 -0800

    Writing to file as tab spaced

--- crawler1.py
+++ crawler1.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Step 1: Sending a HTTP request to a URL
-result = requests.get("http://www.wikicfp.com/cfp/call?conference=big%20data%20&page=2")
+result = requests.get("http://www.wikicfp.com/cfp/call?conference=big%20data%20&page=1")
 src = result.text
 
 # Step 2: Parse the HTML content
@@ -16,9 +16,15 @@ rows = soup.select("body > div.contsec > center > form > table > tbody > tr:nth-
 # Step 4: Write the details of each conference in a file
 # Each element is separated by a tab space
 with open("data.txt", "w") as file:
+    newConf = True
     for row in rows:
+        newConf = not newConf
         tdTags = row.find_all("td", {"align" : "left"})
         string = ""
         for data in tdTags:
             string += data.text + "\t"
-        file.write(string.strip() + "\n")
+        if newConf:
+            file.write("\n" + string.strip())
+        else:
+            file.write("\t" + string.strip())
+    file.close()
